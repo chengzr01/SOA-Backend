6923c9a53448e252ee626d67b7b8daa93966b456
Siyuan Wu <siyuan-w20@mails.tsinghua.edu.cn>
2024-04-16 21:57:59 +0800

    update todo

--- src/query.py
+++ src/query.py
@@ -14,9 +14,11 @@ def respond_to_user_input(
     complete = agent.check_key_info_completeness(user_input)
     if complete:
         query = agent.query_backend(user_input)
-        # TODO: send query to backend
-        
-        return {"front end response": None, "back end response": None}
+        # {"company": "Google", "job_title": "software engineering"}
+        # TODO: get response from backend
+        # res = get_response_from_backend(query)
+        # [{"company": "Google", "job_title": "software engineering"}, {"company": "Facebook", "job_title": "data scientist"}]
+        # return {"front end response": None, "back end response": res}
     else:
         response = agent.respond_frontend(user_input)
         return response
