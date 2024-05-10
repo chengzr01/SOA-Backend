122760d469e850ef1e495f7e994cdb80a15af002
caohanwen <chw20@mails.tsinghua.edu.cn>
2024-05-10 10:49:57 +0800

    add middleware

--- SOA/backend/middleware.py
+++ SOA/backend/middleware.py
@@ -12,17 +12,3 @@ class ExemptCSRFMiddleware:
 
         response = self.get_response(request)
         return response
-from django.urls import resolve
-
-class ExemptCSRFMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        # Check if the request path matches any URL under "accounts/"
-        if request.path.startswith("/accounts/"):
-            # Exempt CSRF protection
-            setattr(request, '_dont_enforce_csrf_checks', True)
-
-        response = self.get_response(request)
-        return response
